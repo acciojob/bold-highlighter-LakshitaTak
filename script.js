40dfa813f5bb395e418e1549a53fe9a77abf16cc
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-24 20:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-function highlight() {
-    //Write your code here
+let boldText = []
 
+function getBoldText() {
+  let paragraph = document.getElementById("para")
+  boldText = paragraph.getElementsByTagName("strong")
+
+}
+getBoldText()
+
+function highlight() {
+  //Write your code here
+  for (let index = 0; index < boldText.length; index++) {
+    boldText[index].style.color = "green"
+  }
+  console.log(boldText)
+
+  console.log("change color")
 
 }
 
 
 function return_normal() {
+
+  for (let index = 0; index < boldText.length; index++) {
+    boldText[index].style.color = "black"
     //Write your code here
 
-    
+  }
+  console.log("back to normal")
 }
